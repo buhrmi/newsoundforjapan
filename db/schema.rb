3c83caa902d401a3c107091df1e45678de38f09f
Stefan Buhrmester <buhrmi@gmail.com>
2020-11-01 12:42:13 +0100

    protect admin area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_112517) do
+ActiveRecord::Schema.define(version: 2020_11_01_113934) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_10_31_112517) do
     t.string "twitch_id"
     t.string "display_name"
     t.string "description"
+    t.boolean "admin"
     t.index ["twitch_id"], name: "index_users_on_twitch_id"
   end
 
