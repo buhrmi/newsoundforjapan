4ca2a0a4f81c34889e9df38565d8ec430979c8ac
Stefan Buhrmester <buhrmi@gmail.com>
2020-11-03 09:48:45 +0100

    stay healthy

--- app/views/components/pages/slugs/covid19.svelte
+++ app/views/components/pages/slugs/covid19.svelte
@@ -6,6 +6,8 @@
 
 <ul>
   <li>DJ equipment will be cleaned and treated with disinfectant after each performance.</li>
-  <li>All guests are requested to wear a mouth and nose cover at all times.</li>
+  <li>All guests and staf are requested to wear a mouth and nose cover at all times.</li>
   <li>Please do not attend any event if your are feeling sick.</li>
 </ul>
+
+<p>Please stay safe, healthy, and hydrated.</p>
